521123d7cfe1b3ad5d30267a6a1788bee610e9a7
pypalkar23 <mandar.palkar94@gmail.com>
2021-04-24 21:33:29 +0530

    Session 2

--- src/golang-book/arrayslicemap/main.go
+++ src/golang-book/arrayslicemap/main.go
@@ -16,6 +16,20 @@ func main() {
 	y[3] = 82
 	y[4] = 83
 	var total float64 = 0
+
+	//shorthand syntax for array declaration
+	z := [5]float64{12, 25, 265, 56, 165}
+	fmt.Println(z)
+
+	//for arrays that does not fit in one line
+	t := [5]float64{
+		93,
+		12,
+		12, //this extra comma is needed as per syntax
+	}
+
+	fmt.Println(t)
+
 	for i := 0; i < 5; i++ {
 		total += y[i]
 	}
@@ -32,4 +46,47 @@ func main() {
 	for _, value := range y {
 		fmt.Println(value)
 	}
+
+	///---Slices Section Start
+	//empty slice-seems useless lets see
+	var slice1 []float64
+	fmt.Println(slice1)
+
+	//slice with size 5
+	slice2 := make([]float64, 5)
+	fmt.Println(slice2)
+
+	//slice with size 5 capacity 10
+	slice3 := make([]float64, 5, 10)
+	slice3[0] = 10
+	fmt.Println(slice3, len(slice3))
+
+	//slice created from array
+	arr := [5]float64{1, 2, 3, 4, 5}
+	slice4 := arr[0:4] //inclusive of start and end
+
+	for i := 0; i < len(slice4); i++ {
+		fmt.Println(slice4[i])
+	}
+
+	//append slice
+	slice5 := []int{1, 2, 3}
+	slice6 := append(slice5, 4, 5)
+	fmt.Println(slice5, slice6) // slice6-> 1,2,3,4,5
+
+	slice7 := []int{1, 2, 3}
+	slice8 := make([]int, 2)
+	copy(slice7, slice8) // will copy only first two elements as slice8 has size 2
+	fmt.Println(slice1, slice2)
+
+	//--slice end
+	//-------maps start---------
+	var map1 map[string]int     //key->string value->int note:this is uninitialized
+	map1 = make(map[string]int) //initialized it here
+	map1["key"] = 10
+	fmt.Println(map1)
+
+	delete(map1, "key")
+	fmt.Println(map1)
+
 }
