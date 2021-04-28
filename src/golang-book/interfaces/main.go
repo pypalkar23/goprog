eb8ee96a6c496805a0922e8ed05e42520e8b2256
mandar.palkar <mandar.palkar@gupshup.io>
2021-04-28 21:17:22 +0530

    Embedded types and Interfaces

---
+++ src/golang-book/interfaces/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	c := Circle{0, 0, 5}
+	fmt.Println(c.area())
+
+	r := Rectangle{0, 0, 10, 10}
+	fmt.Println(r.area())
+
+	fmt.Println(multiArea(&c, &r))
+}
+
+//declaration of interface
+type Shape interface {
+	area() float64
+}
+
+type Circle struct {
+	x, y, r float64
+}
+
+type Rectangle struct {
+	x, y, l, b float64
+}
+
+//to implement an interface for particular struct implement its methods
+func (c *Circle) area() float64 {
+	return math.Pi * c.r * c.r
+}
+
+func (r *Rectangle) area() float64 {
+	return r.l * r.b
+}
+
+func multiArea(shapes ...Shape) float64 {
+	totalArea := 0.0
+	for _, s := range shapes {
+		totalArea += s.area()
+	}
+	return totalArea
+}
--- src/golang-book/structs/main.go
+++ src/golang-book/structs/main.go
@@ -1,5 +1,88 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
+/*
+	type Circle Struct{
+		x float64,
+		t float64,
+		r float64
+	}
+*/
+
+func main() {
+	var c1 Circle
+	c1 = Circle{x: 0, y: 0, r: 5}
+	fmt.Println(c1) //{0,0,5}
+
+	c1.x = -2
+	c1.y = -10
+
+	//if order of the field is known field names can be omitted
+	c2 := Circle{0, 0, 5}
+	fmt.Println(c2)
+
+	c3 := new(Circle) //allocate memory for Circle struct instance returns pointer to the c3
+	fmt.Println(c3)   // &{0,0,0}
+	fmt.Println(*c3)  //{0,0,0}
+
+	c3.x = 10
+	c3.y = 10
+	c3.r = 10
+	fmt.Println(*c3)
+
+	fmt.Println(func_calcArea(c1))
+	fmt.Println(c2.method_area())
+	fmt.Println(c3.method_area())
+
+	p := new(Person)
+	p.name = "Mandar"
+	p.talk()
+
+	android := new(Android)
+	android.P = *p //since new returns pointer
+	android.P.talk()
+	p.name = "Other Name"
+	android.P.talk() // will still print Mandar that p was passed as value not reference
+
+	android1 := Android1{}
+	android1.Person = *p
+	android1.Person.talk()
+	android1.talk() //since android1's embedded type is not named we can call the talk directly on android
+}
+
 type Circle struct {
 	x, y, r float64
+}
+
+//normal function that takes circle instance
+func func_calcArea(c Circle) float64 {
+	return math.Pi * c.r * c.r
+}
+
+//method declaration
+//    receiver
+func (c *Circle) method_area() float64 {
+	return math.Pi * c.r * c.r //notice no *c reference here
+}
+
+type Person struct {
+	name string
+}
+
+func (p *Person) talk() {
+	fmt.Println("Hello, my name is", p.name)
+}
+
+type Android struct {
+	P     Person //Embedded Type
+	Model string
+}
+
+type Android1 struct {
+	Person //another example of Embedded type with no instance name
+	Model  string
 }
