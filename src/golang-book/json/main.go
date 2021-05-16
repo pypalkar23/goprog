cbdb0d251d5fff4c92d868f983ef15ad9b627d5b
pypalkar23 <mandar.palkar94@gmail.com>
2021-05-16 20:18:47 +0530

    json marshalling and unmarshalling

---
+++ src/golang-book/json/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Movie struct {
+	Title  string
+	Year   int  `json:"released"`
+	Color  bool `json:"color,omitempty"`
+	Actors []string
+}
+
+func main() {
+	movies := []Movie{
+		{Title: "Casablanca", Year: 1942, Color: false,
+			Actors: []string{"Humphrey Bogart", "Ingrid Bergman"}},
+		{Title: "Cool Hand Luke", Year: 1967, Color: true,
+			Actors: []string{"Paul Newman"}},
+		{Title: "Bullitt", Year: 1968, Color: true,
+			Actors: []string{"Steve McQueen", "Jacqueline Bisset"}},
+		// ...
+	}
+
+	data, err := json.Marshal(movies)
+	if err != nil {
+		fmt.Printf("Json marshalling failed: %s", err)
+	}
+	fmt.Printf("%s\n", data)
+
+	var titles []struct {
+		Title string
+	}
+
+	var moviesList []Movie
+
+	err1 := json.Unmarshal(data, &titles)
+	if err1 != nil {
+		fmt.Printf("Json marshalling failed: %s", err1)
+	}
+	fmt.Printf("%s\n", titles)
+
+	err2 := json.Unmarshal(data, &moviesList)
+	if err2 != nil {
+		fmt.Printf("Json marshalling failed: %s", err2)
+	}
+	fmt.Println(moviesList)
+}
